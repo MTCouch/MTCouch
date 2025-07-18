1a839bea93a2e2d3a425541236c14947098ec6e1
Carlost1wre3 <carloswendell.t1@gmail.com>
2025-07-17 23:29:59 -0300

    primeiros teste como postgres

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,14 +1,15 @@
-var mysql = require('mysql');
+var pg= require('pg');
 
-var connectMYSQL = function(){
-    return mysql.createConnection({
-        host: 'localhost',
-        database: 'BDusuarios',
-        user: 'root',
-        password: ''
-    });
+var connectPG = function(){
+    return pg.createConnection({
+        host: 'dpg-d1sqkcp5pdvs73cs4eh0-a',
+        database: 'test_vwmf',
+        user: 'test_vwmf_user',
+        password: 'YScHCKYoQdCJmYpMHgvWG3yqPMryc6IY',
+        port: '5432'
+    })
 }
 
 module.exports = function(){
-    return connectMYSQL;
+    return connectPG
 }
