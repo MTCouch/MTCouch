289c3ac22186038aa081f2d4b316e21fdd4dc6e8
Carlost1wre3 <carloswendell.t1@gmail.com>
2025-08-01 19:53:22 -0300

    Portgresql

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,15 +1,5 @@
-var pg= require('pg');
+require("dotenv").config();
+const { neon } = require("@neondatabase/serverless");
+const sql = neon(process.env.DATABASE_URL);
 
-var connectPG = function(){
-    return pg.createConnection({
-        host: 'dpg-d1sqkcp5pdvs73cs4eh0-a',
-        database: 'test_vwmf',
-        user: 'test_vwmf_user',
-        password: 'YScHCKYoQdCJmYpMHgvWG3yqPMryc6IY',
-        port: '5432'
-    })
-}
-
-module.exports = function(){
-    return connectPG
-}
+console.log(usuarios);
